2c706d21d6dd13ae71063bf42e843f914b9a5e17
jose-cleiton <cleitons835@gmail.com>
2022-12-21 16:35:42 -0300

    feat: :sparkles: passando o  constructor() {     this.pokemonDir = path.resolve('.', 'pokemons');   } para o constutor main

--- src/index.js
+++ src/index.js
@@ -1,59 +1,61 @@
 const fs = require('fs').promises;
 
 const path = require('path');
-const pokemonDir = path.resolve('.', 'pokemons');
-
-
 const https = require('https');
 
 class PokemonAPI {
   constructor() {
     this.url = 'https://pokeapi.co/api/v2/pokemon?limit=100';
+    
   }
 
   async getPerson() {
     return new Promise((resolve, reject) => {
-      https.get(this.url, (res) => {
-        let data = '';
-        res.on('data', (chunk) => {
-          data += chunk;
+      https
+        .get(this.url, (res) => {
+          let data = '';
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            try {
+              resolve(JSON.parse(data));
+            } catch (e) {
+              reject(e);
+            }
+          });
+        })
+        .on('error', (e) => {
+          reject(e);
         });
-        res.on('end', () => {
-          try {
-            resolve(JSON.parse(data));
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }).on('error', (e) => {
-        reject(e);
-      });
     });
   }
 }
 
 class Main {
+  constructor() {
+    this.pokemonDir = path.resolve('.', 'pokemons');
+  }
   makePokemonDir = async () => {
     try {
-      const stats = await fs.stat(pokemonDir);
+      const stats = await fs.stat(this.pokemonDir);
       if (!stats.isDirectory()) {
-        throw new Error(`${pokemonDir} is not a directory`);
+        throw new Error(`${this.pokemonDir} is not a directory`);
       }
     } catch (err) {
       if (err.code === 'ENOENT') {
         // The directory does not exist, so we can create it
-        await fs.mkdir(pokemonDir);
+        await fs.mkdir(this.pokemonDir);
       } else {
         // Some other error occurred, so we'll just throw it
         throw err;
       }
     }
   };
-  
+
   start = async () => {
     console.clear();
     await this.makePokemonDir();
-
 
     const starWarsAPI = new PokemonAPI();
 
@@ -63,25 +65,23 @@ class Main {
     } catch (err) {
       console.error(err);
     }
-    
   };
   makePokemonDir = async () => {
     try {
-      const stats = await fs.stat(pokemonDir);
+      const stats = await fs.stat(this.pokemonDir);
       if (!stats.isDirectory()) {
-        throw new Error(`${pokemonDir} is not a directory`);
+        throw new Error(`${this.pokemonDir} is not a directory`);
       }
     } catch (err) {
       if (err.code === 'ENOENT') {
         // The directory does not exist, so we can create it
-        await fs.mkdir(pokemonDir);
+        await fs.mkdir(this.pokemonDir);
       } else {
         // Some other error occurred, so we'll just throw it
         throw err;
       }
     }
   };
-  
 }
 
 const main = new Main();
