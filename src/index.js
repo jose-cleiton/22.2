042d5e63c920212fd528e47d16e0a00a60c7a8f0
jose-cleiton <cleitons835@gmail.com>
2022-12-21 14:05:52 -0300

    Fluxo Assíncrono

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+const fs = require('fs/promises');
+const path = require('path');
+
+class Main {
+  static start() {
+    // Coloque o código da função main aqui
+  }
+}
+
+Main.start();
