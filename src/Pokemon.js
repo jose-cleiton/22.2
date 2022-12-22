3b7469dfd961872d626d9f3db1f845691ef81f36
jose-cleiton <cleitons835@gmail.com>
2022-12-22 13:34:27 -0300

    fix: :sparkles: Transformando em orientaçao a obj

---
+++ src/Pokemon.js
@@ -0,0 +1,42 @@
+const API = require('./API');
+class Pokemon {
+  constructor(api) {
+    this.api = api;
+  }
+
+  async pokemonsImag(nome) {
+    const resul = await this.api.get(`pokemon/${nome}`);
+    const { sprites } = resul;
+    const result = {};
+
+    result.master = [];
+    for (const key in sprites) {
+      const value = sprites[key];
+      if (value !== null && typeof value !== 'object') {
+        result.master.push(value);
+      }
+    }
+
+    result.other = [];
+    for (const key in sprites.other) {
+      const value = sprites.other[key];
+      result.other = result.other.concat(Object.values(value).filter(val => val !== null));
+    }
+
+    result.type = [];
+    for (const key in sprites) {
+      const value = sprites[key];
+      if (value !== null && typeof value !== 'object' && key !== 'versions') {
+        result.type.push(value);
+      }
+    }
+
+    console.log(result);
+  }
+}
+
+
+
+
+
+ module.exports = Pokemon;
--- src/index.js
+++ src/index.js
@@ -1,108 +1,17 @@
-const API = require('./API');
-const api = new API('https://pokeapi.co/api/v2/');
+const Pokemon = require('./Pokemon')
+const API = require('./Api')
 
-const pokemonsImag = async (nome) => {
-  const resul = await api.get(`pokemon/${nome}`);
+class Main {
+  constructor() {
+    this.pokemonAPI = new API('https://pokeapi.co/api/v2/');
+    this.pokemon = new Pokemon(this.pokemonAPI);
+  }
 
-  const { sprites } = resul;
- 
- 
-  const pkm = sprites
-  
-  
-  const result = {};
-
-// Inicialize o array "master"
-result.master = [];
-
-// Itera sobre as chaves do objeto "sprites"
-for (const key in sprites) {
-  const value = sprites[key];
-  
-  // Se o valor não for nulo e não for um objeto, adicione-o ao array "master"
-  if (value !== null && typeof value !== 'object') {
-    result.master.push(value);
+  async start() {
+    console.clear();
+    await this.pokemon.pokemonsImag('bulbasaur');
   }
 }
 
-// Inicialize o array "other"
-result.other = [];
-
-// Itera sobre o objeto "other"
-for (const key in sprites.other) {
-  const value = sprites.other[key];
-  
-  // Adicione as URLs do objeto "value" ao array "other"
-  result.other = result.other.concat(Object.values(value).filter(val => val !== null));
-}
-
-// Inicialize o objeto "type"
-result.type = [];
-
-// Itera sobre as chaves do objeto "sprites"
-for (const key in sprites) {
-  const value = sprites[key];
-  
-  // Se o valor não for nulo, não for um objeto e a chave não for "versions", adicione-o ao array "type"
-  if (value !== null && typeof value !== 'object' && key !== 'versions') {
-    result.type.push(value);
-  }
-}
-
-console.log(result);
-
-  
-
-
-
-
- 
-}
-pokemonsImag('bulbasaur');
-
-// class Main {
-//   constructor(pokemonAPI) {
-//     this.pokemonDir = path.resolve('.', 'pokemons');
-//     this.pokemonAPI = pokemonAPI;
-//   }
-//   makePokemonDir = async () => {
-//     try {
-//       const stats = fs.stat(this.pokemonDir);
-//       if (!stats.isDirectory()) {
-//         throw new Error(`${this.pokemonDir} is not a directory`);
-//       }
-//     } catch (err) {
-//       if (err.code === 'ENOENT') {
-//         // The directory does not exist, so we can create it
-//         await fs.mkdir(this.pokemonDir);
-//       } else {
-//         // Some other error occurred, so we'll just throw it
-//         throw err;
-//       }
-//     }
-//   };
-
-//   start = async () => {
-//     console.clear();
-//     await this.makePokemonDir();
-
-//     try {
-//       const { results } = await this.pokemonAPI.fatchApi();
-//       const itemPokemons = results.map((item) => item.url);
-
-//       const itemPokemon = itemPokemons[0];
-//       const pokemon = await this.pokemonAPI.fatchApi(itemPokemon);
-
-//       await this.pokemonAPI.fatchPokemonImage(pokemon);
-
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-//   makePokemonDir = () => {
-//     fs.mkdirSync(this.pokemonDir, { recursive: true });
-//   };
-// }
-
-// const main = new Main(new PokemonAPI());
-// main.start();
+const main = new Main();
+main.start();
